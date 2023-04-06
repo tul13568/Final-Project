abfd5c90bdddd2ab72c04d20e2837706897787dd
tul13568 <117791901+tul13568@users.noreply.github.com>
2023-04-06 09:55:05 -0400

    Add files via upload

---
+++ Preproposal.py
@@ -0,0 +1,16 @@
+# Preproposal
+
+## What idea(s) do you have for your final project?
+
+- Business Identifier- growing social media platforms such as Instagram and Tiktok are a place for small businesses to promote their companies
+                     -From personal experience, there has been an increase in scammers
+                     - my project idea would be to find a way to verify these accounts and introduce people to small businesses in their area based on what they are looking for
+
+
+## If you plan to collaborate with one or two classmates, what are their names?
+
+As of right now, I would be doing the project on my own
+
+## Do you have any questions of your own?
+
+Would I have to connect the program to any apps such as Instagram or Tiktok to prove that it works? Or just as a test?
